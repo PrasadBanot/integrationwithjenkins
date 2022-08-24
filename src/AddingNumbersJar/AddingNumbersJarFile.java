ceedcca7278a8845af9df9aa468acd20e0579362
unknown <mindc1july18@iiht.tech>
2022-08-24 08:44:00 +0530

    add prasad

---
+++ src/AddingNumbersJar/AddingNumbersJarFile.java
@@ -0,0 +1,12 @@
+package AddingNumbersJar;
+
+public class AddingNumbersJarFile {
+
+	public static void main(String[] args) {
+		int a=66,b=63;
+		int sum =a+b;
+		System.out.println("the sum is:" + sum);
+
+	}
+
+}
